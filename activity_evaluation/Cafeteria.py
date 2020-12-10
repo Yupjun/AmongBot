5f400a9baffe9f11a23ff32a959f478d480384a0
yupjun <yaup22c@naver.com>
2020-12-10 23:26:01 +0900

    cafe garbage

--- activity_evaluation/Cafeteria.py
+++ activity_evaluation/Cafeteria.py
@@ -1,7 +1,19 @@
+import pynput
+import time
+"""
+왼쪽 위를 기준으로 
+x는 가로
+y는 세로
+"""
+
+
+MOUSE_CONTROLLER= pynput.mouse.Controller()
+MOUSE_BUTTON = pynput.mouse.Button
+
 def Upload_Data():
 	do something
 	return something
 
 def Empty_Garbage():
 	do something
-	return something
+	
